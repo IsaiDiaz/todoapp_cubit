6e7e053bb173f5689de6d4a08c2b9dbb58a1b783
IsaiDiaz <jsoza057@gmail.com>
2023-04-21 12:24:34 -0400

    Agregada pantalla de login y home

---
+++ lib/pages/home.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<String> _tasks = [];
+
+  void _addTask() {
+    setState(() {
+      _tasks.insert(0, 'New Task');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Task List'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  iconColor: MaterialStateProperty.all(Colors.black),  
+                  shape: MaterialStateProperty.all(
+                    ContinuousRectangleBorder(
+                      side: BorderSide(color: Colors.grey[300]!),
+                    ),
+                  ),
+                ),
+                onPressed: _addTask,
+                child: const Icon(Icons.add)),
+            ),
+      
+            for (var task in _tasks)
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    border: Border(
+                      right: BorderSide(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      left: BorderSide(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      top: BorderSide(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      bottom: BorderSide(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        task,
+                        style: const TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        'Description',
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
+                  )
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class Login extends StatelessWidget {
+  Login({super.key});
+
+  //controladores de texto de nombre de usuario y contraseña
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(45.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your username';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 32.0),
+              ElevatedButton(
+                onPressed:(){
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Processing Data')),
+                    );
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+                  }
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
